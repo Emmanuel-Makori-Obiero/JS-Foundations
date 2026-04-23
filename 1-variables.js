1f8dd9c37d9eb5a7c5b5973ac658aadc47bf2aad
Emmanuel Makori Obiero <emmanuelmakobiero@gmail.com>
2026-04-23 17:19:26 +0300

    First commit

---
+++ 1-variables.js
@@ -0,0 +1,15 @@
+//a variable is a labelled container for a piece of data, it is like assigning some memory to RAM of a computer to hold the data. e.g score=10 the score is the variable
+/*The life cycle of a variable is
+i. Declaring the variable e.g Score=10 the score is the variable
+ii. Assigning the variable with a value e.g score=10 the value is 10(e.g let score = 10 or const score = 10)
+iii.Accessing the variable to see what is inside
+iv. changing what is in the variable e.g after writing let score = 10 one proceeds to say score = 20...the previous score will overide the first one*/
+//The variables must be case sensitive e.g name and Name are different and also they should be meaningful e.g use the word Name to assign a value of a name and not another variable like s
+/*let and const are used when declaring variables
+let - one can change values and is more prone to bugs.
+const - or constant.is Most preferred since one cannot change a value and is less pprone to errors.*/
+
+//Example of assigning a variable is
+let name = John;
+//or
+const name = John;
