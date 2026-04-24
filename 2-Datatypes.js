489c2b369e2da695abb1ae121dbdb6930571957f
Emmanuel Makori Obiero <emmanuelmakobiero@gmail.com>
2026-04-24 10:41:15 +0300

    Datatypes

---
+++ 2-Datatypes.js
@@ -0,0 +1,9 @@
+//This is a  classification that tells a computer the type of data being entered
+//Some of the data types are:
+/*i. Numbers-these are basically numbers and are used in calculations e.g let age = 10
+ii. strings-This is data entered in quotations e.g let name = "Emmanuel"
+iii. booleans- This are of two types true/false and are mostly used in decision making e.g isrutotwoterm = false
+iv. undefined- This is basically an empty box e.g let y = undefined and a real life example is lets say there is a form where people should fill out their name and somebody forgets to fill out their name.
+To prevent the system from crashing the input is automatically filled and returns an undefined value to show that the property does not exist
+v. Null- This is a box that was intentionally emptied e.g let z = null.. null is mostly considered an object..its application can be in the form lets say the user clicked the clear button in the age field..My app sends the age: null to tell the database to delete the existing value
+*/
