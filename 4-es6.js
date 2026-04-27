dc0b82d46712ff82a5761fabacd61f4c6bfea0d3
Emmanuel Makori Obiero <emmanuelmakobiero@gmail.com>
2026-04-27 08:59:28 +0300

    ES6

---
+++ 4-es6.js
@@ -0,0 +1,16 @@
+//This is an updated version of javascript and also Known as ECMASCRIPT..*NB.Did you know that Ecmascript stands for EUROPEAN COMPUTER MANUFACTURING ASSOCIATION
+/*Some of the advantages brought about by ES6 are:
+i)Scope and reliability - They fixed quirks that lead to errors.
+ii)Scalability- Makes huge amounts of code executable
+iii)Syntactic sugar- Makes complex code easy to write.*/
+
+/*Some of the new features of Es6 are:
+i)Block Scoping - Use of let and const instead of Var e.g. let Name = "Emmanuel" instead of Var Name = "Emmanuel" because Var is too leaky especially in loops
+ii)Arrow Funnctions - use of an arrow made of an equal sign and a greater than sign =>...The arrow means leading to e.g
+lets say we want to double a number instead of using Function(double) {
+Return number*2;}...but his has been replaced by const double = number => number*2
+iii)Template literals - This replaces the complex + with a ${}.e.g 
+const name = "Emmanuel"
+const Statement = `Welcome home ${name}`--New method
+const statement = "Welcome home" +name "pleased to see you"--Old method
+*/
