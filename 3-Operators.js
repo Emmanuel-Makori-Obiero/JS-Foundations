6f295078dca64127d6fe8a79ce2a9f7b569f59e4
Emmanuel Makori Obiero <emmanuelmakobiero@gmail.com>
2026-04-27 07:14:33 +0300

    Operators

---
+++ 3-Operators.js
@@ -0,0 +1,8 @@
+//These are mostly symbols e.g + that receive inputs called operands and process them to the required output
+/*The types of operands are:
+i)Assignment- These assign a value to a variable e.g let name = Emmanuel..the (=) is an assignment operand
+ii)Arithmetic- These perform calculations such as addition and subtraction e.g let Price = CurrentPrice - Discount.. the (-) is an arithmetic operand
+iii)Logical- These perform logical operations whereby they combine multiple conditions and return the complex one.g let coupon = true..let cangetdiscount = (price > 10) & hascoupon.
+iv)Comparison(answer is either true or false) - they are used to compare two values and return a boolean e.g let price = (price > 20).
+*nb/ one should know the difference between == amd ===..===is most preferred becausee it returns an accurate answer
+*/
